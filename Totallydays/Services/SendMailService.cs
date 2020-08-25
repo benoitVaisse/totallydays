400fcb9d2fed16d4e641cb75645c4c65df586ef9
benoit vaisse <benoit.vaisse@gmail.com>
2020-08-25 22:59:05 +0200

    commentaire

--- Totallydays/Services/SendMailService.cs
+++ Totallydays/Services/SendMailService.cs
@@ -7,6 +7,9 @@ using Totallydays.Models;
 
 namespace Totallydays.Services
 {
+    /// <summary>
+    /// classe qui gere les envoie de mail
+    /// </summary>
     public class SendMailService
     {
         private readonly IEmailService _mailService;
@@ -16,6 +19,12 @@ namespace Totallydays.Services
             this._mailService = mailService;
         }
 
+        /// <summary>
+        /// envoie un mail de verification du compte
+        /// </summary>
+        /// <param name="User"></param>
+        /// <param name="Link"></param>
+        /// <returns></returns>
         public async Task sendVeridyEmail(AppUser User, string Link)
         {
             await this._mailService.SendAsync(User.Email, "Email Verification", $"<a href=\"{Link}\">click to verify </a>", true);
