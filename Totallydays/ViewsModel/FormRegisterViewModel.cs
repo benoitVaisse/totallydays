a6c84b1b8502173d703a9f8f93e94f3d44eeddcf
benoit vaisse <benoit.vaisse@gmail.com>
2020-08-25 22:46:03 +0200

    model lié au formulaire

---
+++ Totallydays/ViewsModel/FormRegisterViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Totallydays.ViewsModel
+{
+    public class FormRegisterViewModel
+    {
+
+        [Required]
+        [Display(Name = "Prénom")]
+        public string Firstname { get; set; }
+
+        [Required]
+        [Display(Name = "Nom")]
+        public string Lastname { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Mot de passe")]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirmer le mot de passe")]
+        [Compare("Password", ErrorMessage = "Les mots de passe douvent être identique")]
+        public string ConfirmPassword { get; set; }
+    }
+}
