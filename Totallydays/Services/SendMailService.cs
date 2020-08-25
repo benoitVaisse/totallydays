c76b666c8e031af08b965418e78bd36ced9b07c8
benoit vaisse <benoit.vaisse@gmail.com>
2020-08-25 22:45:10 +0200

    creation du sendAmilService qui nous servira a envoyer des mail

---
+++ Totallydays/Services/SendMailService.cs
@@ -0,0 +1,24 @@
+﻿using NETCore.MailKit.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Totallydays.Models;
+
+namespace Totallydays.Services
+{
+    public class SendMailService
+    {
+        private readonly IEmailService _mailService;
+
+        public SendMailService(IEmailService mailService)
+        {
+            this._mailService = mailService;
+        }
+
+        public async Task sendVeridyEmail(AppUser User, string Link)
+        {
+            await this._mailService.SendAsync(User.Email, "Email Verification", $"<a href=\"{Link}\">click to verify </a>", true);
+        }
+    }
+}
