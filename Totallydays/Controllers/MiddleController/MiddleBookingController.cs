e7234d7cb608553d0e863c5b6d71320628357d73
benoit vaisse <benoit.vaisse@gmail.com>
2020-10-24 18:36:59 +0200

    creatin route pour envoyer un commentaire sur une annonce passé qui na pas encore de commentaire

--- Totallydays/Controllers/MiddleController/MiddleBookingController.cs
+++ Totallydays/Controllers/MiddleController/MiddleBookingController.cs
@@ -5,8 +5,10 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Totallydays.Models;
 using Totallydays.Repositories;
+using Totallydays.Services;
 using Totallydays.ViewsModel;
 
 namespace Totallydays.Controllers.MiddleController
@@ -17,14 +19,17 @@ namespace Totallydays.Controllers.MiddleController
     {
         private readonly BookingRepository _bookinRepository;
         private readonly UserManager<AppUser> _userManager;
+        private readonly CommentService _commentService;
 
         public MiddleBookingController(
             BookingRepository bookingRepo,
-            UserManager<AppUser> userManager
+            UserManager<AppUser> userManager,
+            CommentService commentService
             )
         {
             this._bookinRepository = bookingRepo;
             this._userManager = userManager;
+            this._commentService = commentService;
         }
         [HttpGet("mes-reservations", Name ="my_bookings")]
         public async Task<IActionResult> MyBookings()
@@ -37,6 +42,37 @@ namespace Totallydays.Controllers.MiddleController
             this.ViewBag.formComment = new FormCommentViewModel();
 
             return View();
+        }
+
+
+        [HttpPost("mes-reservations/commentaire/submit", Name = "my_bookings_post_comment")]
+        [Authorize]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> PostComment(FormCommentViewModel model)
+        {
+            AppUser User = await this._userManager.GetUserAsync(this.User);
+            if (ModelState.IsValid)
+            {
+                Booking Booking = await this._bookinRepository.Find(model.BookingId);
+                // si la reservation n'existe pas ou si la réservation ne m'appartient pas 
+                if(Booking == null || Booking.User != User)
+                {
+                    this._errorMessage.Add("Un Problème est survenue lors de l'envoie du commentaire");
+                    this._ajaxFlashessage.Add("error", this._errorMessage);
+
+                    return Json(new { status = "error", messages = this._ajaxFlashessage });
+                }
+
+                await this._commentService.Create(model, User);
+                this._successMessage.Add("Le commentaire a bien été envoyé.");
+                this._ajaxFlashessage.Add("success", this._successMessage);
+                return Json(new { status = "success", message = this._ajaxFlashessage });
+            }
+
+            this.SetErroMessageAjax();
+            this._ajaxFlashessage.Add("error", this._errorMessage);
+
+            return Json(new { status="error", messages = this._ajaxFlashessage });
         }
     }
 }
