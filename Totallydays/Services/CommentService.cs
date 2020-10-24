d14625a82f0af37a0c226924d398e3ef4481deeb
benoit vaisse <benoit.vaisse@gmail.com>
2020-10-24 18:35:18 +0200

    creation service et repo sur les commentaire

---
+++ Totallydays/Services/CommentService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Threading.Tasks;
+using Totallydays.Models;
+using Totallydays.Repositories;
+using Totallydays.ViewsModel;
+
+namespace Totallydays.Services
+{
+    public class CommentService
+    {
+        private readonly CommentRepository _commentRepository;
+
+        public CommentService(
+            CommentRepository commentRepo
+        )
+        {
+            this._commentRepository = commentRepo;
+        }
+
+        public async Task<Comment> Create(FormCommentViewModel model, AppUser User)
+        {
+            Comment Comment = new Comment()
+            {
+                BookingBooking_id = model.BookingId,
+                User_emmiter = User,
+                Content = model.Comment,
+                Rating = int.Parse(model.Rating),
+                Created_at = DateTime.Now
+
+            };
+
+            Comment = await this._commentRepository.CreateAsync(Comment);
+
+            return Comment;
+        }
+    }
+}
--- Totallydays/Startup.cs
+++ Totallydays/Startup.cs
@@ -77,6 +77,7 @@ namespace Totallydays
             services.AddScoped<BookingService, BookingService>();
             services.AddScoped<GoogleMapService, GoogleMapService>();
             services.AddScoped<BedRoomService, BedRoomService>();
+            services.AddScoped<CommentService, CommentService>();
             services.AddScoped<ControllerExtenstionServiceRazor, ControllerExtenstionServiceRazor>();
             // repository 
             services.AddScoped<EquipmentRepository, EquipmentRepository>();
@@ -89,6 +90,7 @@ namespace Totallydays
             services.AddScoped<BookingRepository, BookingRepository>();
             services.AddScoped<UnavailableDateRepository, UnavailableDateRepository>();
             services.AddScoped<BedRoomRepository, BedRoomRepository>();
+            services.AddScoped<CommentRepository, CommentRepository>();
 
 
 
