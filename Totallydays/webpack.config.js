cf5c78e31f7f3b9ed3398c47cee21518b0945250
benoit vaisse <benoit.vaisse@gmail.com>
2020-11-15 17:15:03 +0100

    ajout config webpack

---
+++ Totallydays/webpack.config.js
@@ -0,0 +1,30 @@
+﻿const path = require('path');
+
+module.exports = {
+    entry: __dirname + "/wwwroot/source/app.js",
+    mode: "development",
+    output: {
+        path: path.resolve(__dirname, 'wwwroot/dist'),
+        filename: "bundle.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            ['@babel/preset-env', {
+                                targets: {
+                                    "ie": "11"
+                                }
+                            }]
+                        ]
+                    }
+                }
+            }
+        ]
+    },
+}
