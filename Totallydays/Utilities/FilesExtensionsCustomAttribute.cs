32090769907181e4d0e20212d5c5b4fad605936c
benoit vaisse <benoit.vaisse@gmail.com>
2020-12-07 13:57:21 +0100

    doc

--- Totallydays/Utilities/FilesExtensionsCustomAttribute.cs
+++ Totallydays/Utilities/FilesExtensionsCustomAttribute.cs
@@ -10,23 +10,40 @@ using System.Threading.Tasks;
 
 namespace Totallydays.Utilities
 {
+    /// <summary>
+    /// classe qui va vérifer si l'extension du fichier est conforme aux extensions souhaitées
+    /// </summary>
     public class FilesExtensionsCustomAttribute : ValidationAttribute
     {
         private readonly List<string> _fileExtension;
+
+        /// <summary>
+        /// construction
+        /// </summary>
+        /// <param name="fileExtensions"></param>
         public FilesExtensionsCustomAttribute(string fileExtensions)
         {
+            // on récupère la chaine de caractere des extension sous forme de tableau
             this._fileExtension = fileExtensions.Split(',', StringSplitOptions.RemoveEmptyEntries).ToList();
 
         }
 
+        /// <summary>
+        /// methode de verification
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="ValidationContext"></param>
+        /// <returns></returns>
         protected override ValidationResult IsValid(object value, ValidationContext ValidationContext)
         {
+            //recupération du fichier
             IFormFile File = value as IFormFile;
             if(File != null)
             {
                 string ext = Path.GetExtension(File.FileName);
                 if (!this._fileExtension.Contains(ext))
                 {
+                    // si l'extension du fichier n'est pas bonne
                     return new ValidationResult(string.Format(
                     CultureInfo.CurrentCulture,
                     FormatErrorMessage(ValidationContext.DisplayName)
