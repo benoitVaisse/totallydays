add83c3b0a80b4f8f45fd353d1589136659df761
benoit vaisse <benoit.vaisse@gmail.com>
2021-01-12 14:02:07 +0100

    fix

--- Totallydays/ViewsModel/FormSearchHostingViewModel.cs
+++ Totallydays/ViewsModel/FormSearchHostingViewModel.cs
@@ -14,9 +14,11 @@ namespace Totallydays.ViewsModel
         public string City { get; set; }
 
         [CompareDate("End_Date", "<", ErrorMessage = "La date de début de séjour doit être inferieur à celle de fin")]
+        [DateLessGreaterThan(compare: ">", ErrorMessage = "La Date de début de séjour dois etre superieur à celle de aujourdhui")]
         public DateTime Start_Date { get; set; }
 
         [CompareDate("Start_Date", ">", ErrorMessage = "La date de fin de séjour doit être supéreur à celle de début")]
+        [DateLessGreaterThan(compare: ">", ErrorMessage = "La Date de fin de séjour dois etre superieur à celle de aujourdhui")]
         public DateTime End_Date { get; set; }
 
         [RegularExpression("([0-9]+)", ErrorMessage = "le nombre de personne n'est pas correct")]
